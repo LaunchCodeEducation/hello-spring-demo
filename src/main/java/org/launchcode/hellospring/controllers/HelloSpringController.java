21aa65fa1cdc7b54ba38d34962b700a19b90932b
Chris Bay <chris@chrisbay.net>
2019-11-18 07:05:12 -0600

    Adds handlers with parameters

--- src/main/java/org/launchcode/hellospring/controllers/HelloSpringController.java
+++ src/main/java/org/launchcode/hellospring/controllers/HelloSpringController.java
@@ -2,6 +2,8 @@ package org.launchcode.hellospring.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
@@ -10,10 +12,18 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class HelloSpringController {
 
+    // Responds to /hello?name=LaunchCode
     @GetMapping("hello")
     @ResponseBody
-    public String hello() {
-        return "Hello, Spring!";
+    public String hello(@RequestParam String name) {
+        return "Hello, " + name + "!";
+    }
+
+    // Responds to /hello/LaunchCode
+    @GetMapping("hello/{name}")
+    @ResponseBody
+    public String helloAgain(@PathVariable String name) {
+        return "Hello, " + name + "!";
     }
 
 }
