527cd5d8e9249f71db30cbd978ae79bbd96ddf35
Chris Bay <chris@chrisbay.net>
2019-11-27 07:00:59 -0500

    Adds form template

--- src/main/java/org/launchcode/hellospring/controllers/HelloSpringController.java
+++ src/main/java/org/launchcode/hellospring/controllers/HelloSpringController.java
@@ -7,33 +7,25 @@ import org.springframework.web.bind.annotation.*;
  * Created by Chris Bay
  */
 @Controller
-@ResponseBody
-@RequestMapping("hello")
 public class HelloSpringController {
 
     // Responds to /hello?name=LaunchCode
-    @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST})
+    @RequestMapping(value = "hello", method = {RequestMethod.GET, RequestMethod.POST})
+    @ResponseBody
     public String hello(@RequestParam String name) {
         return "Hello, " + name + "!";
     }
 
     // Responds to /hello/LaunchCode
-    @GetMapping("{name}")
+    @GetMapping("hello/{name}")
+    @ResponseBody
     public String helloAgain(@PathVariable String name) {
         return "Hello, " + name + "!";
     }
 
     @GetMapping("form")
     public String helloForm() {
-        String html = "<html>" +
-                "<body>" +
-                "<form method = 'post' action = '/hello'>" +
-                "<input type = 'text' name = 'name' />" +
-                "<input type = 'submit' value = 'Greet Me!' />" +
-                "</form>" +
-                "</body>" +
-                "</html>";
-        return html;
+        return "form";
     }
 
 }
