5a99d3b314cf58a16267a5775824e53242382389
Carly Langlois <cj.langlois.cl@gmail.com>
2020-08-13 16:42:43 -0500

    rename controller, add form handler and post mapping

---
+++ src/main/java/org/launchcode/hellospring/controllers/HelloController.java
@@ -0,0 +1,53 @@
+package org.launchcode.hellospring.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * Created by Chris Bay
+ */
+@Controller
+public class HelloController {
+
+    //    // Handle request at path /hello
+//    @GetMapping("hello")
+//    @ResponseBody
+//    public String hello() {
+//        return "Hello, Spring!";
+//    }
+
+    @GetMapping("goodbye")
+    @ResponseBody
+    public String goodbye() {
+        return "Goodbye, Spring!";
+    }
+
+    // Handles requests of the form /hello?name=LaunchCode
+    @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST}, value = "hello")
+    @ResponseBody
+    public String helloWithQueryParam(@RequestParam String name) {
+        return "Hello, " + name + "!";
+    }
+
+    // Handles requests of the form /hello/LaunchCode
+    @GetMapping("hello/{name}")
+    @ResponseBody
+    public String helloWithPathParam(@PathVariable String name) {
+        return "Hello, " + name + "!";
+    }
+
+
+    @GetMapping("form")
+    @ResponseBody
+    public String helloForm() {
+        return "<html>" +
+                "<body>" +
+                "<form action = 'hello' method = 'post'>" + // submit a request to /hello
+                "<input type = 'text' name = 'name' >" +
+                "<input type = 'submit' value = 'Greet Me!' >" +
+                "</form>" +
+                "</body>" +
+                "</html>";
+    }
+
+}
