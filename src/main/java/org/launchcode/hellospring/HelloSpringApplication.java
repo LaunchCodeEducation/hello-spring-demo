769e78881abf26123fdb2e8a324529fdd4a0c0c7
Carly Langlois <cj.langlois.cl@gmail.com>
2021-04-02 14:00:14 -0500

    making a change to test pushing tag

--- src/main/java/org/launchcode/hellospring/HelloSpringApplication.java
+++ src/main/java/org/launchcode/hellospring/HelloSpringApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class HelloSpringApplication {
 
-	public static void main(String[] args) {
+	ublic static void main(String[] args) {
 		SpringApplication.run(HelloSpringApplication.class, args);
 	}
 
